c748897c3a9162ef2bb5d0a335f8c3427200e6a9
Dmitry <lexa.gorky@yandex.ru>
2022-10-04 15:17:06 +0300

    added mkfile, tell, show, rm, rename and some helpful funcs

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import os
 import shutil
 
 
+def __getcwd() -> str:
+    return os.getcwd()[os.getcwd().index('maindir'):] + '>'
+
+
+def __getfulname(s: str) -> str:
+    fulname = os.getcwd() + '\\' + s
+    if not s.endswith('.txt'):
+        fulname += '.txt'
+    return fulname
+
+
 def mkdir(name='new_folder') -> None:
     """создание папки"""
     path = os.getcwd() + '\\'
@@ -41,7 +52,7 @@ def cd(name: str, flag=None) -> None:
                 print(path + '>')
             elif name == '..' and not os.getcwd().endswith('maindir'):
                 os.chdir(name)
-                print(os.getcwd()[os.getcwd().index('maindir'):] + '>')
+                print(__getcwd())
             else:
                 print('Папка maindir является корневой.')
         except (FileNotFoundError, ):
@@ -51,9 +62,71 @@ def cd(name: str, flag=None) -> None:
         print(path + '>')
 
 
+def mkfile(name='new_file') -> None:
+    """Создание пустого текстового файла"""
+    name = name + '.txt'
+    fulname = os.getcwd() + '\\' + name
+    if name not in os.listdir(os.getcwd()):
+        open(fulname, 'a').close()
+        print(f"Файл {name} создан.\n" + __getcwd())
+    else:
+        print(f"Файл {name} уже есть в данной директории.\n" + __getcwd())
+
+
+def tell(text: str, file: str) -> None:
+    """Запись строк в существующий файл"""
+    fulname = __getfulname(file)
+    if fulname[fulname.rindex('\\')+1:] in os.listdir(os.getcwd()):
+        with open(fulname, 'a') as f:
+            f.write(text + '\n')
+            print(f"Файл {file} обновлён.\n" + __getcwd())
+    else:
+        print(f"Файла с таким именем нет в данной директории.\n" + __getcwd())
+
+
+def show(file: str) -> None:
+    """Вывод содержимого существующего файла"""
+    fulname = __getfulname(file)
+    if fulname[fulname.rindex('\\')+1:] in os.listdir(os.getcwd()):
+        with open(fulname, 'r') as f:
+            print(f.read() + '\n' + __getcwd())
+    else:
+        print(f"Файла с таким именем нет в данной директории.\n" + __getcwd())
+
+
+def rm(file: str) -> None:
+    """Удаление существующего файла"""
+    fulname = __getfulname(file)
+    try:
+        os.remove(fulname)
+        print(f'Файл {file} удалён\n' + __getcwd())
+    except (FileNotFoundError, ):
+        print(f"Файла с таким именем нет в данной директории.\n" + __getcwd())
+
+
+def copy(file: str, path: str) -> None:
+    """Копирование файла из одной директории в другую"""
+    pass
+
+
+def move(file: str, path: str) -> None:
+    """Перемещение файла из одной директории в другую"""
+    pass
+
+
+def rename(file: str, new_name: str) -> None:
+    """Переменование файла в данной директории"""
+    fulname = __getfulname(file)
+    new_name = __getfulname(new_name)
+    new_name = new_name[new_name.rindex('\\')+1:]
+    if fulname not in os.listdir(os.getcwd()):
+        print(f"Файла с таким именем нет в данной директории.\n" + __getcwd())
+    elif new_name in os.listdir(os.getcwd()):
+        print(f"Другому файлу в данной директории присвоено такое имя.\n" + __getcwd())
+    else:
+        os.rename(fulname, new_name)
+
 
 
 if __name__ == '__main__':
-    os.chdir('maindir')  # файл начинает всегда работать в D:\pythonProject_file_manager
-
-
+    os.chdir('maindir')  # файл начинает всегда работать в D:\pythonProject_file_manager\maindir
